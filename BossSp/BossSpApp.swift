0d903b2cf7ac87b915bffe7cf6bb6ad1d57b6a26
Jeong Dong <eejxdong99@gmail.com>
2022-07-26 15:09:25 +0900

    Create a new project

---
+++ BossSp/BossSpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BossSpApp.swift
+//  BossSp
+//
+//  Created by 이정동 on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BossSpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
