33a9673d4ab03f57f644008dee411be38799fabc
Jeong Dong <eejxdong99@gmail.com>
2022-08-05 15:52:57 +0900

    Feat: Implement Logout

--- BossSp/LoginView.swift
+++ BossSp/LoginView.swift
@@ -20,13 +20,13 @@ struct LoginView: View {
         .resizable()
         .aspectRatio(contentMode: .fit)
 
-      Text("Welcome to Ellifit!")
+      Text("Welcome to BoSS")
         .fontWeight(.black)
         .foregroundColor(Color(.systemIndigo))
         .font(.largeTitle)
         .multilineTextAlignment(.center)
 
-      Text("Empower your elliptical workouts by tracking every move.")
+      Text("지도에 맛집 위치를 표시해서 맛집 정보를 공유하세요.")
         .fontWeight(.light)
         .multilineTextAlignment(.center)
         .padding()
